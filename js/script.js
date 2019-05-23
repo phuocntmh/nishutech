24538340691455758472ba0402d06d04d27d3036
Nguyễn Trọng Minh Hồng Phước <39150891+phuocntmh@users.noreply.github.com>
2019-05-23 13:53:09 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 var frm = $('#register-form');
 frm.submit(function (e) {
-    e.preventDefault();
-    $.ajax({
-        type: 'GET',
-        beforeSend: function(){
-            $("input").prop('disabled', true);
-            $("#submit-form").attr("disabled", true);
-        },
-        url: 'https://script.google.com/macros/s/AKfycbxIKOm6nq0hqI-au3mI6cytiGYst3skND2R9fRtHJWlrbJU3Vqa/exec',
-        data: frm.serialize(),
-        success: function (data) {
-            $('#SuccessModal').modal('show');
-        },
-        error: function (data) {
-            $('#FailModal').modal('show');
-        },
-        complete: function(){
-            $("input").prop('disabled', false);
-            $("#submit-form").attr("disabled", false);
-            $('#register-form')[0].reset();
-        }
-    });
+    var recaptcha = $("#g-recaptcha-response").val();
+    if (recaptcha === "") {
+        e.preventDefault();
+        alert("Vui lòng kiểm tra biểu mẫu. Error: Please check the Recaptcha.");
+    }
+    else {
+        e.preventDefault();
+        $.ajax({
+            type: 'GET',
+            beforeSend: function(){
+                $("input").prop('disabled', true);
+                $("#submit-form").attr("disabled", true);
+            },
+            url: 'https://script.google.com/macros/s/AKfycbxIKOm6nq0hqI-au3mI6cytiGYst3skND2R9fRtHJWlrbJU3Vqa/exec',
+            data: frm.serialize(),
+            success: function (data) {
+                $('#SuccessModal').modal('show');
+            },
+            error: function (data) {
+                $('#FailModal').modal('show');
+            },
+            complete: function(){
+                $("input").prop('disabled', false);
+                $("#submit-form").attr("disabled", false);
+                $('#register-form')[0].reset();
+            }
+        });
+    }
 });
